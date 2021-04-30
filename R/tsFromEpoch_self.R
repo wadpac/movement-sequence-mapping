ef608d0fc73705cf0bd632e703c61a75dfb1623a
Vincent van Hees <vincentvanhees@gmail.com>
2021-04-30 13:38:06 +0200

    minor fixes

--- R/tsFromEpoch_self.R
+++ R/tsFromEpoch_self.R
@@ -14,6 +14,7 @@ tsFromEpoch_self <- function (object, minn) {
   }
   if (any(minn > object$info$nobs)) {
     warning("Timestamp is outside observed time interval")
+  }
   ss = object$info$ts_start + object$info$epoch * (as.integer(minn) -  1)
   even_indices = seq(2, length(ss), by = 2)
   ss[even_indices] = ss[even_indices] + 1
